e529d1ee43ac8429785c76320746e432ff0ac598
PauloWarley <92232479+PauloWarley@users.noreply.github.com>
2022-10-24 23:39:26 -0300

    v0.3 fix

---
+++ aproved_in_inteli/src/pages/api/createbet.js
@@ -0,0 +1,8 @@
+const App = (req, res) => {
+
+    var {nickname, bet} = req.body
+
+    res.status(201).json({response})
+}
+
+export default App
--- whatsapp_bot/driver/whatsapp-bot.py
+++ whatsapp_bot/driver/whatsapp-bot.py
@@ -28,6 +28,7 @@
 # In[20]:
 
 
+import random
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -42,19 +43,66 @@ while len(navegador.find_elements(By.ID,"side")) < 1:
     time.sleep(1)
 
 pessoa = 'Paulo'
-mensagem = 'teste'
-numero = '5531996055862'
-
-texto = urllib.parse.quote(f"Oi {pessoa}! {mensagem}")
-link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
-navegador.get(link)
-while len(navegador.find_elements(By.ID, "side")) < 1:
-    time.sleep(1)
+mensagem = 'Saiu o resultado do Inteli!!'
+numeros = [
     
-while len(navegador.find_elements(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span')) < 1:
-    time.sleep(1)
-navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span').send_keys(Keys.ENTER)
-time.sleep(10)
+]
+
+frases = [
+["Acerte em tudo que puder acertar. Mas não se torture com seus erros.",
+"Paulo Coelho (Manuscrito Encontrado em Accra, 2012)"],
+    
+["Você nunca sabe que resultados virão da sua ação. Mas se você não fizer nada, não existirão.",
+"Mahatma Gandhi"],
+
+["É melhor você tentar algo, vê-lo não funcionar e aprender com isso, do que não fazer nada.",
+"Mark Zuckerberg"],
+
+["A possibilidade de realizarmos um sonho é o que torna a vida interessante.",
+"Paulo Coelho"],
+
+["A vida é uma grande universidade, mas pouco ensina a quem não sabe ser aluno.",
+"Augusto Cury"],
+
+["Por medo de diminuir, deixamos de crescer. Por medo de chorar, deixamos de rir.",
+"Paulo Coelho (Manual do Guerreiro da Luz, 1997)"],
+
+["A persistência é o caminho do êxito.",
+"Charles Chaplin"],
+
+["Quantas coisas perdemos por medo de perder.",
+"Paulo Coelho (Brida, 1990)"]
+,
+["O sucesso nasce do querer, da determinação e persistência em se chegar a um objetivo. Mesmo não atingindo o alvo, quem busca e vence obstáculos, no mínimo fará coisas admiráveis.",
+"José de Alencar"],
+
+["A alegria está na luta, na tentativa, no sofrimento envolvido e não na vitória propriamente dita.",
+"Mahatma Gandhi"],
+
+["Não se deixe intimidar pela opinião dos outros. Só a mediocridade é segura, por isso corra seus riscos e faça o que deseja.",
+"Paulo Coelho (O Aleph, 2011)"],
+
+["A coragem não é ausência do medo; é a persistência apesar do medo.",
+"Desconhecido"]
+]
+
+for i in numeros:
+    numero = i
+    print("Número:", i)
+    
+    frase = random.choice(frases)
+
+    texto = urllib.parse.quote(f"""Oii ! {mensagem}\n "{frase[0]}"\n{frase[1]}""")
+    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
+    
+    navegador.get(link)
+    while len(navegador.find_elements(By.ID, "side")) < 1:
+        time.sleep(1)
+        
+    while len(navegador.find_elements(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span')) < 1:
+        time.sleep(1)
+    navegador.find_element(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span').send_keys(Keys.ENTER)
+    time.sleep(5)
 
 
 
