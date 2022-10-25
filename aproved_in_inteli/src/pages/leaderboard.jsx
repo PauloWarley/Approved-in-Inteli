de498f4237a8549d4a6b129483161686c2a7e8c1
PauloWarley <92232479+PauloWarley@users.noreply.github.com>
2022-10-25 01:48:44 -0300

    v0.3 fix

--- aproved_in_inteli/src/pages/leaderboard.jsx
+++ aproved_in_inteli/src/pages/leaderboard.jsx
@@ -1,10 +1,12 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Input from '../components/InputMask'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import background from '../images/background.png'
+import leaderboard from '../images/leaderboard.png'
 
 const App = () => {
   const [number, setNumber] = useState('')
@@ -85,37 +87,26 @@ const App = () => {
     </Head>
 
     <div className={styles.root}>
-      <header className={styles.cabecalho}>
-        <p className={styles.cabecalho_title}>Olá Candidato (a), <br/><br/> <strong>Seja bem-vindo (a)</strong> 
-         à sua página de resultado do Processo Seletivo Inteli 2023.1</p>
-      </header>
-      <main className={styles.content}>
-        <section className={styles.content_main}>
-            <p>Se você quiser ser notificado quando sair o resultado por WhatsApp, coloque seu numero abaixo.</p>
-            <label htmlFor="content-telefone"></label>
-            <Input className={styles.content_telefone} onChange={(e) => setNumber(e.target.value)} name="número" id="número"/>
-        </section>
-      </main>
-      <section className={styles.section}>
-        <br/>
+        <header className={styles.header}>
+            <ul>
+                <li > Home </li>
+                <li > Leaderboard </li>
+            </ul>
+        </header>
+        <hero className={styles.leaderboard}>
+            <div>
+                <Image src={leaderboard}  />
 
-        <div className={styles.section_quote}>
-            <div className={styles.section_quote}>Boa sorte (controla a ansiedade) e que Ada Lovelace esteja com você :)</div>
-        </div>
-
-        <br/>
-        <div className={styles.section_button}>
-            <button className={styles.section_button} onClick={() => sendPhone(number)} >Enviar meu número</button>
-        </div>
-        <div></div>
-      </section>  
+            </div>
+        </hero>
     </div>
+
     <div className={styles.background}>
         <Image src={background} />
         <div style={{ color: "white", position: "relative", bottom: '30px', 
           left: '5px',
-          fontFamily:"'Courier New', Courier, monospace;"}}>
-          Desenvelopado by: Paulo Warley and Keylla || Aplicativo não possui vínculo com a INTELI!
+          fontFamily:"Courier New"}}>
+          {/* Desenvelopado by: Paulo Warley and Keylla || Aplicativo não possui vínculo com a INTELI! */}
         </div>
     </div>
 
