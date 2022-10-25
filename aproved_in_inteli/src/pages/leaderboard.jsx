ef1e5834ad491da10a2fbeaa80ac7bb3e24fa814
PauloWarley <92232479+PauloWarley@users.noreply.github.com>
2022-10-25 02:22:22 -0300

    v0.3 fix

--- aproved_in_inteli/src/pages/leaderboard.jsx
+++ aproved_in_inteli/src/pages/leaderboard.jsx
@@ -9,58 +9,6 @@ import background from '../images/background.png'
 import leaderboard from '../images/leaderboard.png'
 
 const App = () => {
-  const [number, setNumber] = useState('')
-  const [modal, setModal] = useState('')
-
-
-  useEffect(() => {
-    // console.log({number})
-  }
-
-  )
-
-  function modalConclusion(id){
-    var texto = [
-      'Cadastro Realizado Com Sucesso!',
-      'Erro ao Cadastrar!',
-      'Número já Cadastrado!'
-    ]
-
-    return(
-      <div id={'background'} style={{
-        position: 'absolute',
-        top: '0',
-        left: '0',
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'rgb(20,20,20, 0.4)',
-        display: 'flex',
-        flexDirection: 'center',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: '5px'
-      }} onClick={(e)=> e.target.id == 'background'?setModal(<></>):''}>
-        <div style={{
-          width: '40%',
-          height: '20%',
-          backgroundColor: 'white',
-          fontSize: '30px',
-          fontWeight: 'bold',
-          fontFamily: "'Courier New', Courier, monospace",
-          color: '#2c263d',
-          textAlign: 'center',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: '10px'
-        }}>
-          {texto[id]}
-        </div>
-      </div>
-    )
-  }
-
-
   return (
     <>
     <Head>
