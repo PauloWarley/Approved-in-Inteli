35c0ffd89db718816e2c2719cefa8d014961923e
Paulo - Adicel <paulo@ADICEL.LOCAL>
2022-11-03 10:24:08 -0300

    site refatorado v2.0 fix

--- aproved_in_inteli/src/components/SecondSection/index.jsx
+++ aproved_in_inteli/src/components/SecondSection/index.jsx
@@ -9,7 +9,7 @@ function SecondSection() {
         <PageSection>
             <ContentWrapper>
                 <ImgContainer>
-                    <Image alt="rounded-arabesque" src="./images/rounded-arabesque.png" />
+                    <Image width={"100%"} height={"100%"} alt="rounded-arabesque" src="/images/rounded-arabesque.png" />
                 </ImgContainer>
                 <LineWrapper>
                     <MainText>
@@ -21,7 +21,7 @@ function SecondSection() {
                     </TextComponent>
                 </LineWrapper>
                 <XComponent>
-                    <Image alt="x" src="./images/x.png" />
+                    <Image width={"100%"} height={"100%"} alt="x" src="/images/x.png" />
                 </XComponent>
                 <LineWrapper>
                     <TextComponent>
@@ -53,7 +53,7 @@ function SecondSection() {
                     </Counter>
                 </LineWrapper>
                 <Mockup>
-                    <Image alt="mockup" src="./images/mockup.png" />
+                    <Image width={"100%"} height={"100%"} alt="mockup" src="/images/mockup.png" />
                 </Mockup>
             </ContentWrapper>
         </PageSection>
--- aproved_in_inteli/src/components/ThirdSection/index.jsx
+++ aproved_in_inteli/src/components/ThirdSection/index.jsx
@@ -21,10 +21,10 @@ function ThirdSection() {
                 </TextComponent>
                 <Maintext id="quem-somos">
                     <span>Apresentando o time de milhões em 3...2...1</span>
-                    <Image alt="thirdsection" src="./images/thirdsection.png" />
+                    <Image width={"100%"} height={"100%"} alt="thirdsection" src="/images/thirdsection.png" />
                 </Maintext>
                 <PersonContainer>
-                    <Image alt="paulo" src="./images/paulo.png" />
+                    <Image width={"100%"} height={"100%"} alt="paulo" src="/images/paulo.png" />
                     <PersonText>
                         <span>Paulo Warley</span>
                         <p>FullStack Dev, idealizador do <strong>InteliMeResponde</strong> e responsável por juntar essa equipe maravilhosa.</p>
@@ -35,10 +35,10 @@ function ThirdSection() {
                         <span>Keylla Oliveira</span>
                         <p>{"Designer UX/UI (com um pé no Front-end) e criadora de todo o design."}</p>
                     </PersonText>
-                    <Image alt="Keylla" src="./images/Keylla.png" />
+                    <Image width={"100%"} height={"100%"} alt="Keylla" src="/images/Keylla.png" />
                 </PersonContainer>
                 <PersonContainer>
-                    <Image alt="mauricio" src="./images/mauricio.png" />
+                    <Image width={"100%"} height={"100%"} alt="mauricio" src="/images/mauricio.png" />
                     <PersonText ref={myRef}>
                         <span>Maurício Azevedo</span>
                         <p>FullStack Dev, nosso anjo do React JS e tudo que envolva Front-end.</p>
